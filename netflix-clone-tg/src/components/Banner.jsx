b23d5432ca54842681d1e82a0747bdebb7b4fe29
chen605 <chow_chen_1996@hotmail.com>
2021-05-25 12:50:21 +0200

    added scroll effects

--- netflix-clone-tg/src/components/Banner.jsx
+++ netflix-clone-tg/src/components/Banner.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../style.css';
 
-import axios from '../axios';
+import axios from 'axios';
 import ReactPlayer from 'react-player/youtube';
 import { AiFillCloseCircle, AiOutlineCheck } from 'react-icons/ai';
 import {
@@ -10,7 +10,12 @@ import {
   VscUnmute,
   VscMute,
 } from 'react-icons/vsc';
-import { BsPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import {
+  BsPlayFill,
+  BsFillPauseFill,
+  BsChevronLeft,
+  BsChevronRight,
+} from 'react-icons/bs';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectMoviePopup, setMoviePopupRedux } from '../feature/userSlice';
 
@@ -22,23 +27,30 @@ import requests from '../Requests';
 const Banner = () => {
   const dispatch = useDispatch();
   const pop = useSelector(selectMoviePopup);
+  const button = document.getElementById('slide');
 
+  //LOGO KEY
   const API_KEY = '00c655f5cf699862386184d892b7378f';
 
   const [movie, setMovie] = useState([]);
   const [active, setActive] = useState(false);
-  const [title, setTitle] = useState('');
-  const [movieId, setMovieId] = useState(null);
-  const [genre, setGenre] = useState('');
-  const [company, setCompany] = useState('');
+  const [title, setTitle] = useState([]);
+  const [movieId, setMovieId] = useState([]);
+  const [genre, setGenre] = useState([]);
+  const [company, setCompany] = useState([]);
   const [play, setPlay] = useState(true);
   const [mute, setMute] = useState(true);
+  const [trailer, setTrailer] = useState([]);
 
   // tijdelijke API call
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(requests.fetchTrending);
+      const request = await axios.get(
+        `https://api.themoviedb.org/3${requests.fetchTrending}`
+      );
       setMovie(request.data?.results);
+      setMovieId(request.data?.results[0].id);
+
       return request;
     }
     fetchData();
@@ -46,7 +58,9 @@ const Banner = () => {
 
   useEffect(() => {
     async function fetchGenre() {
-      const genre = await axios.get(`${requests.fetchGenre}`);
+      const genre = await axios.get(
+        `https://api.themoviedb.org/3${requests.fetchGenre}`
+      );
       setGenre(genre.data?.genres);
       setCompany(genre.data.production_companies[0].name);
       return genre;
@@ -57,13 +71,25 @@ const Banner = () => {
   useEffect(() => {
     async function fetchTitle() {
       const movieTitle = await axios.get(
-        `http://webservice.fanart.tv/v3/movies/460465?api_key=${API_KEY}`
+        `http://webservice.fanart.tv/v3/movies/${movieId}/?api_key=${API_KEY}`
       );
+      console.log(movieTitle);
       setTitle(movieTitle.data?.hdmovielogo[0]);
       return movieTitle;
     }
     fetchTitle();
-  }, []);
+  }, [movieId]);
+
+  useEffect(() => {
+    async function fetchTrailer() {
+      const movieTrailer = await axios.get(
+        `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=d4e2448287553d83842f860fea84e802&language=en-US`
+      );
+      setTrailer(movieTrailer?.data.results[0].key);
+      return movieTrailer;
+    }
+    fetchTrailer();
+  }, [movieId]);
 
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + '...' : string;
@@ -86,7 +112,7 @@ const Banner = () => {
         style={{
           backgroundSize: 'cover',
           backgroundPosition: 'center center',
-          backgroundImage: `url("https://image.tmdb.org/t/p/original/${
+          backgroundImage: `url("https://image.tmdb.org/t/p/original${
             movie[0]?.backdrop_path || movie[0]?.poster__path
           }")`,
         }}
@@ -96,7 +122,7 @@ const Banner = () => {
             <ReactPlayer
               playing={true}
               className="react-player"
-              url="https://www.youtube.com/watch?v=lFDVL1e8WM4"
+              url={`https://www.youtube.com/watch?v=${trailer}`}
               width="100%"
               height="100%"
             />
@@ -149,7 +175,7 @@ const Banner = () => {
                 playing={play}
                 muted={mute}
                 className="pop-up__react-player"
-                url="https://www.youtube.com/watch?v=lFDVL1e8WM4"
+                url={`https://www.youtube.com/watch?v=${trailer}`}
                 width="100%"
                 height="100%"
               />
@@ -229,18 +255,31 @@ const Banner = () => {
         Top 20
       </h2>
 
-      <div className="banner__contents__slider">
+      <div className="banner__contents__slider" id="slider">
+        <button
+          onClick={() => (document.getElementById(`slider`).scrollLeft -= 220)}
+          style={{ position: 'absolute', marginTop: '3%', left: '3%' }}
+        >
+          <BsChevronLeft />
+        </button>
+        <button
+          onClick={() => (document.getElementById(`slider`).scrollLeft += 220)}
+          style={{ position: 'absolute', marginTop: '3%', left: '7%' }}
+        >
+          <BsChevronRight />
+        </button>
+
         {movie.map((image, index) => (
           <Test key={image.id} imageUrl={`${image?.poster_path}`} num={index} />
         ))}
       </div>
 
       <div>
-        <Row genre={Eighties} type="80's" />
-        <Row genre={Nineties} type="90's" />
-        <Row genre={Zeroes} type="00's" />
-        <Row genre={Disney} type="Disney" />
-        <Row genre={Actor} type="Acteur" />
+        <Row genre={Eighties} type="80's" id="8" />
+        <Row genre={Nineties} type="90's" id="9" />
+        <Row genre={Zeroes} type="00's" id="10" />
+        <Row genre={Disney} type="Disney" id="11" />
+        <Row genre={Actor} type="Acteur: Keanu Reeves" id="12" />
       </div>
     </div>
   );
--- netflix-clone-tg/src/components/Row.jsx
+++ netflix-clone-tg/src/components/Row.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import '../style.css';
 
-const Row = ({ genre, type }) => {
+const Row = ({ genre, type, id }) => {
+  const button = document.getElementById('slide');
+  // const button2 = document.getElementById('slide2');
+
+  // button.onClick = function () {
+  //   ;
+  // };
   return (
     <div className="container">
       <h2>{type && type}</h2>
-      <div className="row">
+      <button
+        onClick={() =>
+          (document.getElementById(`slider${id}`).scrollLeft -= 200)
+        }
+      >
+        <BsChevronLeft />
+      </button>
+      <button
+        onClick={() =>
+          (document.getElementById(`slider${id}`).scrollLeft += 200)
+        }
+        style={{ marginLeft: '3%' }}
+      >
+        <BsChevronRight />
+      </button>
+
+      <div className="row" id={`slider${id}`}>
         {genre.map((movie) => (
           <img
             key={movie.id}
